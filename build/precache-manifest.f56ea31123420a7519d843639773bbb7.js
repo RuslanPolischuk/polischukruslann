60f5a0f25c1dea2ae778e41e0cd1ce6f71904bd5
Vadim146 <ruslanpolischuk12@gmail.com>
2023-10-06 12:09:49 +0300

    title

---
+++ build/precache-manifest.f56ea31123420a7519d843639773bbb7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "934cb5ba7f141c9da417",
+    "url": "/polischukruslann/static/css/main.4f32aa2f.chunk.css"
+  },
+  {
+    "revision": "934cb5ba7f141c9da417",
+    "url": "/polischukruslann/static/js/main.892d29b3.chunk.js"
+  },
+  {
+    "revision": "7627ec565b04469184f2",
+    "url": "/polischukruslann/static/js/runtime~main.97881e8d.js"
+  },
+  {
+    "revision": "855aa4ea852ddc7cffa3",
+    "url": "/polischukruslann/static/js/2.99273fb7.chunk.js"
+  },
+  {
+    "revision": "7f3356909a68ac8df278",
+    "url": "/polischukruslann/static/css/3.ba39f3ae.chunk.css"
+  },
+  {
+    "revision": "7f3356909a68ac8df278",
+    "url": "/polischukruslann/static/js/3.87dfb49c.chunk.js"
+  },
+  {
+    "revision": "bac76212bf0cf751445a",
+    "url": "/polischukruslann/static/js/4.8411be94.chunk.js"
+  },
+  {
+    "revision": "e459e47eba5547c9b763",
+    "url": "/polischukruslann/static/js/5.62987f7a.chunk.js"
+  },
+  {
+    "revision": "e165465fea2cd3a71a85",
+    "url": "/polischukruslann/static/js/6.d942bc59.chunk.js"
+  },
+  {
+    "revision": "24d2018092c17fe3f50d",
+    "url": "/polischukruslann/static/js/7.5d873537.chunk.js"
+  },
+  {
+    "revision": "3975f0ead04826aed9aa3c3090bc2b32",
+    "url": "/polischukruslann/index.html"
+  }
+];
